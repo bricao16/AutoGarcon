9ab581a81403b4f51b2272171cf123b8a0c952f5
gafar123 <amud6981@stthomas.edu>
2020-02-20 14:47:31 -0600

    Android sucks

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+//testing cause android studio sucks
 public class MainActivity extends AppCompatActivity {
 
     @Override
