d6f8c67133a40e576e8b34459f40fd2b55760b0d
Gafar Amuda <amud6981@stthomas.edu>
2020-02-21 10:07:31 -0600

    Login and Main Activity Complete

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 //testing cause android studio sucks
 //Testing because I am very cool
 //test2
 public class MainActivity extends AppCompatActivity {
+    Button logOut;
+    FirebaseAuth authentication;
+    private  FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logOut = findViewById(R.id.logOut);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent signIn = new Intent(MainActivity.this, Login.class);
+                startActivity(signIn);
+
+            }
+        });
+
+
+
     }
 }
