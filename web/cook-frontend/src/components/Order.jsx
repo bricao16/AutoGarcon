3cfe430b98522b1e7018fc33be215bc37aefc71d
Steffan <muso9194@stthomas.edu>
2020-02-25 20:30:19 -0600

    Added react-bootstrap

--- web/cook-frontend/src/components/Order.jsx
+++ web/cook-frontend/src/components/Order.jsx
@@ -1,20 +1,43 @@
 import React from "react";
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 function Order(props) {
     return (
-        <div style={orderStyle}>
-            <div style={topStyle}>
-                <p style={{margin: "0", padding: "5px"}}>{props.id}</p>
-            </div>
-            {
-                props.order.items.map((item, key) => (
-                    <div style={itemStyle}>
-                        <p style={{margin: "0", padding: "0.8em"}}>{item.quantity}x</p>
-                        <p style={{margin: "0", padding: "0.8em"}}>{item.title}</p>
-                    </div>
-                ))
-            }
-        </div>
+        <Col sm={4} className="p-.5">
+            {/* <div style={orderStyle}>
+                <div style={topStyle}>
+                    <p style={{margin: "0", padding: "5px"}}>{props.id}</p>
+                </div>
+                {
+                    props.order.items.map((item, key) => (
+                        <div style={itemStyle}>
+                            <p style={{margin: "0", padding: "0.8em"}}>{item.quantity}x</p>
+                            <p style={{margin: "0", padding: "0.8em"}}>{item.title}</p>
+                        </div>
+                    ))
+                }
+            </div> */}
+
+            <Card className="text-center">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    {/* <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                    </Card.Text> */}
+                    {
+                    props.order.items.map((item, key) => (
+                        <div style={itemStyle}>
+                            <p style={{margin: "0", padding: "0.8em"}}>{item.quantity}x</p>
+                            <p style={{margin: "0", padding: "0.8em"}}>{item.title}</p>
+                        </div>
+                    ))
+                }
+                </Card.Body>
+                <Card.Footer className="text-muted">(Custom footer)</Card.Footer>
+            </Card>
+        </Col>
     )
 }
 
