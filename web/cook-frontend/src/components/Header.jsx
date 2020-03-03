59192951b9c3fc4e4cabe21fd4feb56ca373351c
Leah Emerson <leahemerson98@gmail.com>
2020-03-03 17:44:19 -0600

    add logo

--- web/cook-frontend/src/components/Header.jsx
+++ web/cook-frontend/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logoImage from "../assets/logo.png"
+import logoImage from "../assets/AG2.png"
 
 function Header() {
     // This could be put in a CSS file
     const style = {
         display: "flex",
         fontSize: "2em",
-        borderBottom: "solid grey 2px",
+        borderBottom: "solid #1022644 2px",
         padding: "5px 0 5px 10px",
         alignItems: "center",
         fontWeight: "300"
@@ -14,8 +14,8 @@ function Header() {
 
     return (
         <header style={style}>
-            <img src={logoImage} width="auto" height="35px" alt="waiter" />
-            <p style={{margin: 0, marginLeft: "10px"}}>Auto-Garcon</p>
+            <img src={logoImage} width="auto" height="150vw" alt="Auto-Garcon" />
+            {/*<p style={{margin: 0, marginLeft: "10px"}}>Auto-Garcon</p>*/}
         </header>
     );
 }
