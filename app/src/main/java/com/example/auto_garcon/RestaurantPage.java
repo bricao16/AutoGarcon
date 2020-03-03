fc81aa5d55a9882cce933279655a36c2cd3c2c55
HeesukJin <koch8697@stthomas.edu>
2020-03-03 00:20:56 -0600

    trying to figure out restaurant menus

--- app/src/main/java/com/example/auto_garcon/RestaurantPage.java
+++ app/src/main/java/com/example/auto_garcon/RestaurantPage.java
@@ -7,13 +7,22 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuLayout;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -36,10 +45,13 @@ public class RestaurantPage extends AppCompatActivity implements NavigationView.
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
 
+    private static final String TAG = "RestaurantPage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_page);
+
         dbStore=FirebaseFirestore.getInstance();
         docRef = dbStore.collection("Menu").document("Items");
         drawerLayout = findViewById(R.id.restaurant_main);
@@ -54,12 +66,74 @@ public class RestaurantPage extends AppCompatActivity implements NavigationView.
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-
         listView = findViewById(R.id.menu_list);
         initData();
         listAdapter = new ExpandableMenuAdapater(this, listDataHeader, listHash);
         listView.setAdapter(listAdapter);
 
+        SwipeMenuListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("ASDF");
+        list.add("ASDF");
+        list.add("ASDF");
+
+        ArrayAdapter adapter = new ArrayAdapter(RestaurantPage.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_shopping_cart);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Log.d(TAG, "onMenuItemClick: clicked item0 " + index);
+                        break;
+                    case 1:
+                        Log.d(TAG, "onMenuItemClick: clicked item1 " + index);
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
     }
 
     @Override
