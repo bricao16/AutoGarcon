7c94289be3f4a74b7f8cbafcb42191c3ba6dd87f
EmmaSinn <57824139+EmmaSinn@users.noreply.github.com>
2020-03-03 13:10:53 -0600

    Fixed small errors

--- database/ExampleAPIEndpoint.js
+++ database/ExampleAPIEndpoint.js
@@ -6,26 +6,21 @@ app.get('/users/', (req,res)) => {
      //Handler for id
      if(req.query.id) {
           //Put list of ids into array
-          let ids = req.quesry.id.split(",");
-          if (first) {
-               first = false;
-               query = query + "WHERE (username = ?";
-               parameters.push(ids[0];
-               for (let j = 1; j < ids.length; j++) {
-                    query = query + "OR username = ?";
-                    parameters.push(ids[j];
-               } //for
-               query = query + ")";
-          } else {
-               query = query + " AND (username = ?";
-               parameters.push(ids[0]);
-               for(let j = 1; j < ids.length; j++) {
-                    query = query + " OR username = ?";
-                    parameters.push(ids[j]);
-               } //for
-               query = query + ")";
-          } // else
-     } //if(req.query.id)
+          first = false;
+          query = query + "WHERE (username = ?";
+          parameters.push(ids[0];
+          for (let j = 1; j < ids.length; j++) {
+               query = query + "OR username = ?";
+               parameters.push(ids[j]);
+          } //for
+          query = query + ")";
+          query = query + " AND (username = ?";
+          parameters.push(ids[0]);
+          for(let j = 1; j < ids.length; j++) {
+               query = query + " OR username = ?";
+               parameters.push(ids[j]);
+          } //for
+          query = query + ")";
           
      query = "SELECT"..//Look at prepared statements
      res = db.all(query);
