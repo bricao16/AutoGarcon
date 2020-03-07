c04e887fce63497d5da896bceae1b841f4dea901
Gafar Amuda <amud6981@stthomas.edu>
2020-03-07 02:16:34 -0600

    Login page is being re worked for backend

--- app/src/main/java/com/example/auto_garcon/Login.java
+++ app/src/main/java/com/example/auto_garcon/Login.java
@@ -3,6 +3,7 @@ package com.example.auto_garcon;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,32 +13,41 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.JsonObject;
 
 public class Login extends AppCompatActivity {
-    EditText emailId;
-    EditText password;
-    Button buttonSignIn;
-    TextView textViewSignUp;
-    FirebaseAuth authentication;
-    private FirebaseAuth.AuthStateListener authenticateListener;
+   private EditText emailId;
+   private EditText password;
+   private Button buttonSignIn;
+   private TextView textViewSignUp;
+   private Account currentAccount;
+   private AccountManager accountManager;
+   // FirebaseAuth authentication;
+   // private FirebaseAuth.AuthStateListener authenticateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        authentication = FirebaseAuth.getInstance();
+        //  authentication = FirebaseAuth.getInstance();
         emailId = findViewById(R.id.email);
         password = findViewById(R.id.password);
         buttonSignIn = findViewById(R.id.signUp);
         textViewSignUp = findViewById(R.id.loginLink);
 
-        authenticateListener = new FirebaseAuth.AuthStateListener() {
+       /* authenticateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = authentication.getCurrentUser();
@@ -52,7 +62,7 @@ public class Login extends AppCompatActivity {
 
                 }
             }
-        };
+        };*/
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +80,10 @@ public class Login extends AppCompatActivity {
                 else if(email.isEmpty() && passwd.isEmpty()){
                     Toast.makeText(Login.this,"Feilds are Empty!", Toast.LENGTH_SHORT).show();
                 }
-                else if (!(email.isEmpty() && passwd.isEmpty())){
-                    authentication.signInWithEmailAndPassword(email,passwd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                else if (!(email.isEmpty() && passwd.isEmpty())) {
+
+
+                    /*authentication.signInWithEmailAndPassword(email,passwd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
@@ -83,7 +95,7 @@ public class Login extends AppCompatActivity {
                                 finish();
                             }
                         }
-                    });
+                    });*/
                 }
                 else{
                     Toast.makeText(Login.this, "Error Occurred", Toast.LENGTH_SHORT);
@@ -99,8 +111,24 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+    private void sendGetRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="50.19.176.137:8000/menu";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+    }
     protected void onStart(){
         super.onStart();
-        authentication.addAuthStateListener(authenticateListener);
     }
 }
