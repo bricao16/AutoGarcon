6693aada6d38645c1bb50b08bf4d2128675a2eb0
bricao16 <48219334+bricao16@users.noreply.github.com>
2020-03-09 19:49:27 -0500

    Add files via upload

--- web/cook-frontend/src/App.js
+++ web/cook-frontend/src/App.js
@@ -1,42 +1,39 @@
 import React from 'react';
 import './App.css';
-import Nav from 'react-bootstrap/Nav';
-import Header from "./components/Header"
 import Orders from "./components/Orders"
 import MTasks from "./components/MTasks"
 import MLogin from "./components/MLogin";
 import CLogin from "./components/CLogin";
-import SignUp from "./components/SignUp";
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Login from "./components/Login"
 import {
     BrowserRouter as Router,
     Switch,
     Link,
     Route
   } from "react-router-dom";
+  
+var req  = new XMLHttpRequest();
+
+var menu;
+
+req.open("GET", "http://50.19.176.137:8000/menu", true);
+req.send();
+
+req.onload = function(){
+	//console.log(JSON.parse(req.response));
+	menu = JSON.parse(req.response);
+	console.log(menu);
+}
 
 function App() {
- return (
+  return (
     <Router>
-      <Header/>
+      {/*<Header/>*/}
       <div style={backgroundStyle}>
         <Container fluid>
           <Row>
-            <Col sm={4} className="pt-3 px-3" style={navColStyle}>
-              <Nav defaultActiveKey="/" className="flex-column rounded" style={sectionStyle}>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/cook">Cook</Nav.Link>
-                <Nav.Link href="/manager">Manager</Nav.Link>
-                <Nav.Link eventKey="disabled" disabled>
-                  Analytics
-                </Nav.Link>
-              </Nav>
-            </Col>
-
             <Col className="pt-3 px-3">
               <div className="rounded" style={sectionStyle}>
                 <Switch>
@@ -55,17 +52,21 @@ function App() {
                   <Route path="/login_cook">
                     <CLogin/>
                   </Route>
-                  <Route path="/sign_up">
-                    <SignUp/>
+                  <Route path="/statistics">
+                    <MTasks/>
                   </Route>
+                  <Route path="/menu" 
+                        render={(props) => <MTasks {...props} content={"menu"}/>} />
+           
+                  <Route path="/hours" 
+                  render={(props) => <MTasks {...props} content={"hours"}/>} />
                 </Switch>
-                <footer style={footerStyle}>Powered by Auto Garcon</footer>
               </div>
             </Col>
-
           </Row>
         </Container>
       </div>
+      <footer style={footerStyle}>Powered by Auto Garcon</footer>
     </Router>      
   );
 }
@@ -76,26 +77,24 @@ function Home() {
   return (
     <div>
 
-            <div style={homeStyle}>
-              <h2> Welcome to Auto Garcon </h2>
-              <br/>
-              <ul style={{'list-style':'none'}}>
-                <li><Link to='/login_manager'> 
-                  <button type="button"  style = {buttonStyle}>
-                      Manager
-                  </button>
-                </Link></li>
-                <br/>
-                <li><Link to='/login_cook'>
-                  <button type="button" style = {buttonStyle}>
-                      Cook
-                  </button>
-                </Link></li>
-              </ul>
+      <div style={homeStyle}>
+        <h2> Welcome to Auto-Garcon </h2>
+        <br/>
+        <div style={{'list-style':'none'}}>
+          <li><Link to='/login_manager'> 
+            <button type="button" class="btn btn-info btn-lg">
+                Manager Portal
+            </button>
+          </Link></li>
+          <br/>
+          <li><Link to='/login_cook'>
+            <button type="button" class="btn btn-info btn-lg">
+                Cook Portal
+            </button>
+          </Link></li>
+        </div>
 
-              <hr />
-
-            </div>
+      </div>
 
     </div>
   );
@@ -118,7 +117,9 @@ function Manager() {
 }
 
 var backgroundStyle = {
-  'background-color': '#f1f1f1'
+  'background-color': '#f1f1f1',
+  'flex-grow': '1',
+  'height': '100%'
 }
 
 var sectionStyle = {
@@ -126,31 +127,16 @@ var sectionStyle = {
   'height': '100%'
 }
 
-var navColStyle = {
-  'max-width': '200px'
-}
-
 var footerStyle = {
+  'background-color': '#f1f1f1',
   'padding-bottom': '5px',
   'padding-right': '12px',
+  'padding-top': '12px',
   'text-align': 'right'
 }
 var homeStyle = {
-  'height' : '100vh',
   'fontWeight': '300',
-  'width' : '80vw',
   'text-align' : 'center',
   'list-style': 'none',
-  'background-color': '#ffffff',
   'text-size': '50pt'
 };
-var buttonStyle = {
-  'width': '30vw',
-  'fontWeight': '350',
-  'border': 'solid 3px',
-  'color': '#a8a7a',
-  'background-color': 'rgba(11, 101, 138,.75)',
-  'border-color' : 'rgba(11, 101, 138)',
-  'height' : '7vh'
-  
-}
