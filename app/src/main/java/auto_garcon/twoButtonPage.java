d14fd9b28980b1d73efc54045c0e109628278c1d
HeesukJin <koch8697@stthomas.edu>
2020-03-17 03:01:07 -0500

    tem

--- app/src/main/java/auto_garcon/twoButtonPage.java
+++ app/src/main/java/auto_garcon/twoButtonPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.auto_garcon.R;
+
 public class twoButtonPage extends AppCompatActivity {
 
     @Override
