ba6bcb94942023c3b1f85f5e18a672ee05f03da3
Gafar Amuda <47370297+gafar123@users.noreply.github.com>
2020-03-17 02:57:35 -0500

    added third homepage

---
+++ app/src/main/java/auto_garcon/twoButtonPage.java
@@ -0,0 +1,14 @@
+package auto_garcon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class twoButtonPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_button_page);
+    }
+}
