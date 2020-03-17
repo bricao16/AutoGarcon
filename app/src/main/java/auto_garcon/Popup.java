59f535d1f870d21cfbcfa3cf053e4d631c644ced
HeesukJin <koch8697@stthomas.edu>
2020-03-17 00:32:52 -0500

    made blank popup

---
+++ app/src/main/java/auto_garcon/Popup.java
@@ -0,0 +1,34 @@
+package auto_garcon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+import com.example.auto_garcon.R;
+
+public class Popup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * .7), (int)(height * .6));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+    }
+}
