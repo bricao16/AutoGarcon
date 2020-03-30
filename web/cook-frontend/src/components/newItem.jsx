64dc3fd6e3a51dfa725a9fd1c374835e9f77b0b3
Leah Emerson <leahemerson98@gmail.com>
2020-03-30 12:38:59 -0600

    Menu editing functionality
    Menu Item  Edit page with pre-filled of what is already there. Changed check box to toggle.
    
    Need to add/fix:
    -Checkbox to be toggled correctly based on previous state of in stock/ out of stock
    -Delete button
    -Verify POST request working when database has endpoint
    - click into menu card to see more details such as description and photo

--- web/cook-frontend/src/components/newItem.jsx
+++ web/cook-frontend/src/components/newItem.jsx
@@ -1,15 +1,54 @@
 import React from "react";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-
+import '../../node_modules/pretty-checkbox/dist/pretty-checkbox.min.css';
 /* 
   This component is to allow the manager to 
   create a new item. It returns a form object that allows
   for several options and fields to be submitted.  There
   are no helper functions
 */
-function NewItem(props){
-    return ( 
+class NewItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /* Used for connecting to Menu in database */
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+        method: 'POST',
+        data:data
+    };
+
+    fetch('http://50.19.176.137:8000/menu/123', requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: false,
+            menuJSON: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+}
+
+  render(){
+    if(this.props.prefill.type === "default")
+    {
+      return ( 
         <Container>
           <div>
             <Container fluid>
@@ -17,42 +56,98 @@ function NewItem(props){
                 <Container fluid>
                   <div>
                     <form>
-                      <div class="form-group">
-                        <input class="form-control" type="text" id = "itemName" placeholder="Name">
+                      <div className="form-group">
+                        <input className="form-control" type="text" id = "itemName" placeholder={this.props.prefill.menu[0]}>
                         </input>
                        </div>
-                       <div class="form-group">
-                        <input class="form-control" type="text" id = "itemCategory" placeholder="Category">
+                       <div className="form-group">
+                        <input className="form-control" type="text" id = "itemCategory" placeholder={this.props.prefill.menu[1].category}>
                         </input>
                        </div>
-                        <div class="form-group">
-                          <input class="form-control" type="text" id = "itemCalories" placeholder="Calories">
+                        <div className="form-group">
+                          <input className="form-control" type="text" id = "itemCalories" placeholder={this.props.prefill.menu[1].calories}>
                           </input>
                        </div>
-                       <div class="form-group">
-                          <div class="input-group mb-2 mr-sm-2">
-                            <div class="input-group-prepend">
-                              <div class="input-group-text">$</div>
+                       <div className="form-group">
+                          <div className="input-group mb-2 mr-sm-2">
+                            <div className="input-group-prepend">
+                              <div className="input-group-text">$</div>
                             </div>
-                            <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Price">
+                            <input type="text" className="form-control" id="inlineFormInputGroupUsername2" placeholder={this.props.prefill.menu[1].price}>
                             </input>
                           </div>
                       </div>
-                      <div class="form-group">
-                        <label for="itemDescription">Description</label>
-                        <textarea class="form-control" id="itemDescription" rows="3"></textarea>
+                      <div className="form-group">
+                        <label htmlFor="itemDescription">Description</label>
+                        <textarea className="form-control" id="itemDescription" rows="3"></textarea>
                       </div>
-                      <div class="form-group">
-                        <label for="exampleFormControlFile1">Picture</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                      <div className="form-group">
+                        <label hmtlfor="exampleFormControlFile1">Picture</label>
+                        <input type="file" className="form-control-file" id="exampleFormControlFile1">
                         </input>
                       </div>
-                      <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="itemInStock">
-                        </input>
-                        <label class="form-check-label" for="exampleCheck1">Currently in stock</label>
+                      <div class="pretty p-switch p-fill">
+                          <input type="checkbox" defaultChecked/>
+                          <div class="state">
+                              <label placeholder = {this.props.prefill.menu[1].in_stock}>Currently in stock</label>
+                          </div>
                       </div>
-                      <button type="submit" class="btn btn-primary">Submit</button>
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </Container>
+              </Col>
+            </Container> 
+          </div>
+        </Container>
+      );
+    }
+    else{
+      return ( 
+        <Container>
+          <div>
+            <Container fluid>
+              <Col className="pt-3 px-3">
+                <Container fluid>
+                  <div>
+                    <form>
+                      <div className="form-group">
+                        <input className="form-control" type="text" id = "itemName" defaultValue={this.props.prefill.menu[0]}>
+                        </input>
+                       </div>
+                       <div className="form-group">
+                        <input className="form-control" type="text" id = "itemCategory" defaultValue={this.props.prefill.menu[1].category}>
+                        </input>
+                       </div>
+                        <div className="form-group">
+                          <input className="form-control" type="text" id = "itemCalories" defaultValue={this.props.prefill.menu[1].calories}>
+                          </input>
+                       </div>
+                       <div className="form-group">
+                          <div className="input-group mb-2 mr-sm-2">
+                            <div className="input-group-prepend">
+                              <div className="input-group-text">$</div>
+                            </div>
+                            <input type="text" className="form-control" id="inlineFormInputGroupUsername2" defaultValue={this.props.prefill.menu[1].price}>
+                            </input>
+                          </div>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="itemDescription">Description</label>
+                        <textarea className="form-control" id="itemDescription" rows="3"></textarea>
+                      </div>
+                      <div className="form-group">
+                        <label hmtlfor="exampleFormControlFile1">Picture</label>
+                        <input type="file" className="form-control-file" id="exampleFormControlFile1">
+                        </input>
+                      </div>
+                      <div class="pretty p-switch p-fill">
+                          <input type="checkbox" defaultChecked/>
+                          <div class="state">
+                              <label placeholder = {this.props.prefill.menu[1].in_stock}>Currently in stock</label>
+                          </div>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                   </div>
                 </Container>
@@ -61,6 +156,8 @@ function NewItem(props){
           </div>
         </Container>
       ); 
-}
+    }
+  }
 
+}
 export default NewItem;
