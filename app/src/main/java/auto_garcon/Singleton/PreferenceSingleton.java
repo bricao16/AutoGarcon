95fc852c8049d22790fcf7ad6f031756eacf04f4
Gafar Amuda <47370297+gafar123@users.noreply.github.com>
2020-03-30 01:00:23 -0500

    shopping cart stuff

--- app/src/main/java/auto_garcon/Singleton/PreferenceSingleton.java
+++ app/src/main/java/auto_garcon/Singleton/PreferenceSingleton.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.example.auto_garcon.R;
+import com.google.gson.Gson;
+
+import auto_garcon.Cart_OrderHistory.ShoppingCart;
 
 public class PreferenceSingleton {
     private SharedPreferences sharedPreferences;
@@ -50,5 +53,31 @@ public class PreferenceSingleton {
 
         editor.putString(context.getString(R.string.pref_auth_token),authToken);
         editor.commit();
+    }
+    public ShoppingCartSingleton getShoppingCart(){
+        // this gson object will be used to convert our json string into a java object
+        Gson gson = new Gson();
+        //here we extract the json String from our sharedPrefrence file
+        String stringJson  =sharedPreferences.getString("com.example_preference_Shopping_cart","Object");
+
+        //here we convert the json String to our Java Object
+        ShoppingCartSingleton toBeReturned = gson.fromJson(stringJson,ShoppingCartSingleton.class);
+        return  toBeReturned;
+    }
+
+    public void setShoppingCart(ShoppingCartSingleton shoppingCart){
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //this gson object is used to convert our java object into a json String
+        Gson gson = new Gson();
+
+        String stringJson =  gson.toJson(shoppingCart);
+
+        // here we write the json object that represents our java object into our shared prefrence file
+        editor.putString("Object",stringJson);
+        //save edits
+        editor.commit();
+
     }
 }
