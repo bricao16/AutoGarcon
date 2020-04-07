651b94399e561a25b8fc181f4fcb9907d01078cb
HeesukJin <koch8697@stthomas.edu>
2020-04-07 00:05:07 -0500

    made change password activity

--- app/src/main/java/auto_garcon/AccountStuff/Account.java
+++ app/src/main/java/auto_garcon/AccountStuff/Account.java
@@ -1,26 +1,14 @@
 package auto_garcon.AccountStuff;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.auto_garcon.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
-import auto_garcon.HomeStuff.Home;
-import auto_garcon.InitialPages.Login;
-import auto_garcon.Cart_OrderHistory.OrderHistory;
-import auto_garcon.Cart_OrderHistory.ShoppingCart;
-import auto_garcon.InitialPages.QRcode;
 import auto_garcon.Singleton.SharedPreference;
 
 public class Account extends AppCompatActivity {
@@ -33,5 +21,15 @@ public class Account extends AppCompatActivity {
         setContentView(R.layout.activity_account);
 
         pref = new SharedPreference(this);
+
+        TextView changePassword = findViewById(R.id.change_password);
+
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePassword = new Intent(Account.this, PasswordChange.class);
+                startActivity(changePassword);
+            }
+        });
     }
 }
