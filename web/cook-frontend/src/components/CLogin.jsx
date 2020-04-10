ec871aea6dae3a0ec1b3be18377d9ac7bef5fda3
smusoke <steffan.musoke@sportngin.com>
2020-04-10 15:47:12 -0500

    Added error handlers for login pages

--- web/cook-frontend/src/components/CLogin.jsx
+++ web/cook-frontend/src/components/CLogin.jsx
@@ -12,6 +12,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {Redirect} from "react-router-dom";
+import Alert from 'react-bootstrap/Alert';
 
 /*this is the login component for the cook
 view. Asks for the email address, password and logs in if the user and correct password
@@ -41,10 +42,12 @@ export default class SignIn extends React.Component {
 	  
 	  this.state = {
 	    email: '',
-		passwd:'',
-		redirect: false
+      passwd:'',
+      redirect: false,
+      show: false
 	  };
-	  
+    
+    this.handleShow = this.handleShow.bind(this);
 	  this.handleSubmit = this.handleSubmit.bind(this);
 	  this.handleEmail = this.handleEmail.bind(this);
 	  this.handlePasswd = this.handlePasswd.bind(this);
@@ -73,20 +76,19 @@ export default class SignIn extends React.Component {
 	  };
 	  fetch('http://50.19.176.137:8000/staff/login', requestOptions)
 		.then(async response => {
-			const data = await response.json();
-			
-			if(!response.ok){
-				const error = (data && data.message) || response.status;
-				return Promise.reject(error);
-			}
-			this.setState({redirect: true});
-			
-		
+      await response;
+      
+      if (response.status !== 200) {this.handleShow(response);}
+      else {
+        this.setState({show: false});
+        this.setState({redirect: true});
+      }
 		})
 		.catch(error =>{
-			
-			this.setState({redirect: false});
-			console.error("There was an error!", error);	
+      this.setState({alertVariant: 'danger'});
+      this.setState({response: "Unknown error"});
+      this.setState({redirect: false});
+			console.error("There was an error!", error);
 		});
   }
   
@@ -98,22 +100,52 @@ export default class SignIn extends React.Component {
 	  this.setState({passwd: event.target.value});
   }
 
+  /* Used to show the correct alert after failing to log in */
+  handleShow(response) {
+    var text;    
+
+    response.text()
+      .then((res) => {
+        text = res;
+        this.setState({alertVariant: 'danger'});
+
+        if (text) {
+          this.setState({response: text});
+        }
+        else {
+          this.setState({response: 'Failed to login for unknown reason'});
+        }
+
+        this.setState({show: true});
+      })
+  }
+
   render(){
 	if(this.state.redirect === true){
 	  return <Redirect to='/cook'/>
 	}  
 	return (
-  	  <Container component="main" maxWidth="xs">
+  	  <Container component="main" maxWidth="xs" className="p-3">
 		  <CssBaseline />
         <div className={useStyles.paper}>
+
+        <Alert show={this.state.show} variant={this.state.alertVariant}>
+          {this.state.response}
+        </Alert>
+
+        <div style={{'text-align':'center'}}>
           {/* Lock icon on top */}
-          <Avatar className={useStyles.avatar}>
-          <LockOutlinedIcon />
-          </Avatar>
-          {/* Cook Sign In Title on top of page*/}
+          <div style={{'display': 'inline-block'}}>
+            <Avatar className={useStyles.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+          </div>
+          {/* Manager Sign In Title */}
           <Typography component="h1" variant="h5">
-          Cook Sign In
+            Manager Sign In
           </Typography>
+        </div>
+
           <form className={useStyles.form} noValidate>
           <TextField onChange = {this.handleEmail}
             value = {this.state.email}
