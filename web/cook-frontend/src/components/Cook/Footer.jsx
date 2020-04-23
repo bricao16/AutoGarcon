3e5565c1d2ecfb022d84cd1ef0ec1fd97c377518
Alex Palumbo <palu3492@stthomas.edu>
2020-04-23 12:20:30 -0500

    Added logo to footer

--- web/cook-frontend/src/components/Cook/Footer.jsx
+++ web/cook-frontend/src/components/Cook/Footer.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import moment from 'moment'; // Used for getting and formatting time
+import logoImage from "../../assets/AutoGarconLogoHome.png";
 
 // React hook component
 function Footer() {
@@ -18,14 +19,36 @@ function Footer() {
   }
 
   return (
-    <Container fluid style={footerStyle} className="p-2">
-      <h1>{time}</h1>
+    <Container fluid style={footerStyle} className="py-2">
+      <Row>
+        <Col className="d-flex" style={logoContainerStyle}>
+            <Image src={logoImage} height="30px"/>
+            <h1 style={titleStyle} className="mb-0 ml-2">AutoGarcon</h1>
+        </Col>
+        <Col>
+          <h1 style={timeStyle}>{time}</h1>
+        </Col>
+        <Col className="d-none d-md-block">
+        </Col>
+      </Row>
     </Container>
   );
 }
 
 const footerStyle = {
   background: 'white',
+};
+
+const logoContainerStyle = {
+  alignItems: 'center'
+};
+
+const titleStyle = {
+  fontFamily: 'Kefa',
+  fontSize: '2em'
+};
+
+const timeStyle = {
   textAlign: 'center'
 };
 
