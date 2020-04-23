c63e968ab76ff4b56d15ee4cec03c734684b0b32
HeesukJin <koch8697@stthomas.edu>
2020-04-22 20:09:23 -0500

    fixed buttons on order history

--- app/src/main/java/auto_garcon/cartorderhistory/OrderHistory.java
+++ app/src/main/java/auto_garcon/cartorderhistory/OrderHistory.java
@@ -55,7 +55,7 @@ public class OrderHistory extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_order_history);
 
         String temp;
-        final String url = "http://50.19.176.137:8000/customer/history/"+pref.getUser().getUsername();
+        final String url = "http://50.19.176.137:8000/customer/history/" + pref.getUser().getUsername();
         JSONObject obj = new JSONObject();//json object that will be sent as the request parameter
         final StringRequest getRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                 @Override
@@ -63,61 +63,59 @@ public class OrderHistory extends AppCompatActivity implements NavigationView.On
 
                     if(response.equals("No order history for this customer")){
                         setContentView(R.layout.emptyorders);
-
                     }
                     else {
                         setContentView(R.layout.activity_order_history);
                     }
                     Toast.makeText(OrderHistory.this,response+"hmmmm",Toast.LENGTH_LONG).show();
 
+                    //creating side nav drawer
+                    DrawerLayout drawerLayout = findViewById(R.id.order_history_main);// associating xml objects with the java Object equivalent
+                    Toolbar toolbar = findViewById(R.id.xml_toolbar);// associating xml objects with the java Object equivalent
+                    NavigationView navigationView = findViewById(R.id.navigationView);// associating xml objects with the java Object equivalent
+                    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(OrderHistory.this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
 
+                    drawerLayout.addDrawerListener(toggle);
+                    toggle.syncState();
+                    navigationView.setNavigationItemSelectedListener(OrderHistory.this);
+
+                    BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);// associating xml objects with the java Object equivalent
+
+                    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+                            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                    switch (item.getItemId()) {
+                                        case R.id.action_scan:
+                                            Intent QRcode = new Intent(getBaseContext(), QRcode.class);
+                                            startActivity(QRcode);
+                                            return true;
+                                        case R.id.action_home:
+                                            Intent home = new Intent(getBaseContext(), Home.class);
+                                            startActivity(home);
+                                            return true;
+                                        case R.id.action_cart:
+                                            Intent shoppingCart = new Intent(getBaseContext(), ShoppingCart.class);
+                                            startActivity(shoppingCart);
+                                            return true;
+                                    }
+                                    return false;
+                                }
+                            };
+
+                    bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
                 }
             },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(OrderHistory.this,error.getMessage()+"hmmmm",Toast.LENGTH_LONG).show();
-
                     }
                 }
 
         );
         VolleySingleton.getInstance(OrderHistory.this).addToRequestQueue(getRequest);// sending the request to the database
 
-        //creating side nav drawer
-        DrawerLayout drawerLayout = findViewById(R.id.order_history_main);// associating xml objects with the java Object equivalent
-        Toolbar toolbar = findViewById(R.id.xml_toolbar);// associating xml objects with the java Object equivalent
-        NavigationView navigationView = findViewById(R.id.navigationView);// associating xml objects with the java Object equivalent
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
 
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);// associating xml objects with the java Object equivalent
-
-        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.action_scan:
-                                Intent QRcode = new Intent(getBaseContext(), QRcode.class);
-                                startActivity(QRcode);
-                                return true;
-                            case R.id.action_home:
-                                Intent home = new Intent(getBaseContext(), Home.class);
-                                startActivity(home);
-                                return true;
-                            case R.id.action_cart:
-                                Intent shoppingCart = new Intent(getBaseContext(), ShoppingCart.class);
-                                startActivity(shoppingCart);
-                                return true;
-                        }
-                        return false;
-                    }
-                };
-
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
 
     //onClick for side nav bar
