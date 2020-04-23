1b8843e8259039fcf8874d6b5e9f68bed0ac72b9
Gafar Amuda <47370297+gafar123@users.noreply.github.com>
2020-04-22 20:38:09 -0500

    fixing ui

--- app/src/main/java/auto_garcon/cartorderhistory/OrderHistory.java
+++ app/src/main/java/auto_garcon/cartorderhistory/OrderHistory.java
@@ -21,6 +21,7 @@ import com.example.auto_garcon.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -56,55 +57,26 @@ public class OrderHistory extends AppCompatActivity implements NavigationView.On
         pref = new SharedPreference(this);
 
         String temp;
-        final String url = "http://50.19.176.137:8000/customer/history/" + pref.getUser().getUsername();
+        final String url = "http://50.19.176.137:8000/customer/history/"+pref.getUser().getUsername();
         JSONObject obj = new JSONObject();//json object that will be sent as the request parameter
         final StringRequest getRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
                 @Override
                 public void onResponse(String response) {
 
                     if(response.equals("No order history for this customer")){
                         setContentView(R.layout.emptyorders);
+
                     }
                     else {
+                        JsonParser parser = new JsonParser();
+                        JsonObject json =(JsonObject) parser.parse(response);
+
+
+                        Log.d("213",json.toString());
                         setContentView(R.layout.activity_order_history);
                     }
                     Log.d("asdff",response);
-                    //creating side nav drawer
-                    DrawerLayout drawerLayout = findViewById(R.id.order_history_main);// associating xml objects with the java Object equivalent
-                    Toolbar toolbar = findViewById(R.id.xml_toolbar);// associating xml objects with the java Object equivalent
-                    NavigationView navigationView = findViewById(R.id.navigationView);// associating xml objects with the java Object equivalent
-                    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
-
-                    drawerLayout.addDrawerListener(toggle);
-                    toggle.syncState();
-                    navigationView.setNavigationItemSelectedListener(this);
-
-                    BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);// associating xml objects with the java Object equivalent
-
-                    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-                            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                                    switch (item.getItemId()) {
-                                        case R.id.action_scan:
-                                            Intent QRcode = new Intent(getBaseContext(), QRcode.class);
-                                            startActivity(QRcode);
-                                            return true;
-                                        case R.id.action_home:
-                                            Intent home = new Intent(getBaseContext(), Home.class);
-                                            startActivity(home);
-                                            return true;
-                                        case R.id.action_cart:
-                                            Intent shoppingCart = new Intent(getBaseContext(), ShoppingCart.class);
-                                            startActivity(shoppingCart);
-                                            return true;
-                                    }
-                                    return false;
-                                }
-                            };
-
-                    bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-                    //Toast.makeText(OrderHistory.this,response,Toast.LENGTH_LONG).show();
-
                     //creating side nav drawer
                     DrawerLayout drawerLayout = findViewById(R.id.order_history_main);// associating xml objects with the java Object equivalent
                     Toolbar toolbar = findViewById(R.id.xml_toolbar);// associating xml objects with the java Object equivalent
@@ -132,21 +104,26 @@ public class OrderHistory extends AppCompatActivity implements NavigationView.On
                                         case R.id.action_cart:
                                             Intent shoppingCart = new Intent(getBaseContext(), ShoppingCart.class);
                                             startActivity(shoppingCart);
-                                            return true;
                                     }
                                     return false;
                                 }
                             };
 
                     bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+                    //Toast.makeText(OrderHistory.this,response,Toast.LENGTH_LONG).show();
+
+
                 }
             },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(OrderHistory.this,error.getMessage()+"hmmmm",Toast.LENGTH_LONG).show();
+                        Toast.makeText(OrderHistory.this, error.getMessage() + "hmmmm", Toast.LENGTH_LONG).show();
+
                     }
                 }
+
 
         );
         VolleySingleton.getInstance(OrderHistory.this).addToRequestQueue(getRequest);// sending the request to the database
