19cb7ee1402591fee3fb6299d7e0add6a22a7dad
smusoke <steffan.musoke@sportngin.com>
2020-04-28 12:59:14 -0500

    Expand menu item

--- web/cook-frontend/src/components/Manager/Menu/MenuItem.jsx
+++ web/cook-frontend/src/components/Manager/Menu/MenuItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 
 /*
     This Prop is used to render the cards of the Manager Menu page.
@@ -17,78 +18,113 @@ import Card from 'react-bootstrap/Card';
 */
 class MenuItem extends React.Component {
   constructor(props) {
-        super(props);
-        this.state = {
+    super(props);
+    this.state = {};
+  }
 
-        };
+  getStockState(in_stock){
+    if(in_stock === 0) {
+      return "Out of Stock";
     }
-    getStockState(in_stock){
-        if(in_stock === 0)
-        {
-            return "Out of Stock";
-        }
-        else{
-            return "In Stock";
-        }
+    else {
+      return "In Stock";
     }
-    //callback to newitemform when clicked edit button
-    NewItemForm(e) {
-        if (typeof this.props.onNew === 'function') {
-          var props = {
-            type: "existing",
-            item_id: this.props.menu[1].item_id,
-            menu: {
-              "name": this.props.menu[0],
-              "category": this.props.menu[1].category,
-              "price" : this.props.menu[1].price,
-              "calories": this.props.menu[1].calories,
-              "in_stock": this.props.menu[1].in_stock
-            }
-          }
-          
-          this.props.onNew(props);
-        }
-    }
-    render(){
-        if(this.props.menu[1].category === this.props.category)
-        {
-           return(
-                <Card className="text-center m-2" style={itemStyle}> { /* a header is the item name and a clickable edit pencil */}
+  }
 
-                    <Card.Header style={cardHeaderStyle}>{this.props.menu[0]}
-                    </Card.Header>
-                    <div onClick={() => this.NewItemForm()} style={editButtonStyle} className='p-1'>
-                      <a> <i className='fas fa-edit'></i> </a>
+  handleModalClose = () => this.setState({ModalShow: false});
+  handleModalShow = () => this.setState({ModalShow: true});
+
+  //callback to newitemform when clicked edit button
+  NewItemForm(e) {
+    if (typeof this.props.onNew === 'function') {
+      var props = {
+        type: "existing",
+        item_id: this.props.menu[1].item_id,
+        menu: {
+          "name": this.props.menu[0],
+          "category": this.props.menu[1].category,
+          "price" : this.props.menu[1].price,
+          "calories": this.props.menu[1].calories,
+          "in_stock": this.props.menu[1].in_stock
+        }
+      }
+        
+      this.props.onNew(props);
+    }
+  }
+
+  render(){
+      if(this.props.menu[1].category === this.props.category)
+      {
+          return(
+          <>
+            <Modal show={this.state.ModalShow} onHide={this.handleModalClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>{this.props.menu[0]}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div class="container">
+                  <div class="row">
+                    <div class="col">
+                      <div style={modalImageStyle}>
+                        <img src="https://restaurantden.com/wp-content/uploads/2017/09/free-stock-food-photography-websites.jpg" alt="Test stock image" class="img-fluid rounded float-left"></img>
+                      </div>
                     </div>
-                    <Card.Body>
-                      <p style={{margin: "0", padding: "0.3em"}}>${this.props.menu[1].price} </p>
-                      <p style={{margin: "0", padding: "0.3em"}}>Calories: {this.props.menu[1].calories} </p>
-                      <p style={{margin: "0", padding: "0.3em"}}>{this.props.menu[1].picture} </p>
-                      <i> <p style={{margin: "0", padding: "0.3em"}}>{this.getStockState(this.props.menu[1].in_stock)} </p></i>
-                    </Card.Body>
-                </Card>
-            ) 
-        }
-        else{
-            return (
-                <p></p>
-             )
-        }
-    }
+                    <div class="col">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{this.getStockState(this.props.menu[1].in_stock)}</li>
+                        <li class="list-group-item">${this.props.menu[1].price}</li>
+                        <li class="list-group-item">Calories: {this.props.menu[1].calories}</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+            </Modal>
+            
+            <Card className="text-center m-2" style={itemStyle}>
+
+                <Card.Header onClick={this.handleModalShow} style={cardHeaderStyle}>
+                  {this.props.menu[0]}
+                </Card.Header>
+                <div onClick={() => this.NewItemForm()} style={editButtonStyle} className='p-1'>
+                  <a> <i className='fas fa-edit'></i> </a>
+                </div>
+                <Card.Body onClick={this.handleModalShow} style={{'cursor':'pointer'}}>
+                  <p style={{margin: "0", padding: "0.3em"}}>${this.props.menu[1].price} </p>
+                  <p style={{margin: "0", padding: "0.3em"}}>Calories: {this.props.menu[1].calories} </p>
+                  <i> <p style={{margin: "0", padding: "0.3em"}}>{this.getStockState(this.props.menu[1].in_stock)} </p></i>
+                </Card.Body>
+            </Card>
+          </>
+        ) 
+      }
+      else{
+        return (
+          <p></p>
+        )
+      }
+  }
 }
+
 const cardHeaderStyle = {
   'backgroundColor': '#0b658a',
   'color': '#ffffff',
-  'fontFamily': 'Kefa'
+  'fontFamily': 'Kefa',
+  'cursor':'pointer'
 };
 const itemStyle = {
   'width':'200px'
+};
+const modalImageStyle = {
+  'max-width': '200px',
+  'max-height': '200px'
 }
 const editButtonStyle = {
   'cursor':'pointer',
   'color': 'white',
   'position': 'absolute',
   'right': '0'
-}
+};
 
 export default MenuItem;
