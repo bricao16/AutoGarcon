42a069ef0beead364429cbae812d0a185b8a3b41
bricao16 <48219334+bricao16@users.noreply.github.com>
2020-05-13 18:04:40 -0500

    Creation of new restaurant works

--- web/cook-frontend/src/components/SignUp.jsx
+++ web/cook-frontend/src/components/SignUp.jsx
@@ -8,7 +8,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import https from 'https';
 import axios from 'axios';
 import Link from '@material-ui/core/Link';
@@ -53,8 +53,12 @@ class SignUp extends React.Component {
     this.state = {
       restaurant_name: '',
       restaurant_address: '',
-      email: '',
       contact_num: '',
+
+      email: '',
+      opening: '',
+      closing: '',
+      cuisine: '',
       redirect: false,
       show: false,
       //restaurant_id: cookies.get('mystaff').restaurant_id,
@@ -96,7 +100,7 @@ class SignUp extends React.Component {
 
     //if any of the values necessary are not filled out
     if (this.state.restaurant_name === '' || this.state.restaurant_address === '' ||
-      this.state.contact_num === '' || this.state.email === '' ) {
+      this.state.contact_num === '' || this.state.email === '') {
       this.setState({ alertVariant: 'danger' });
       this.setState({ response: "All fields are required" });
       this.setState({ redirect: false });
@@ -111,15 +115,15 @@ class SignUp extends React.Component {
       this.setState({ show: true });
       return;
     }
-     //verify restaurant address
-     if (this.state.restaurant_address.length > 50) {
+    //verify restaurant address
+    if (this.state.restaurant_address.length > 50) {
       this.setState({ alertVariant: 'danger' });
       this.setState({ response: "Restaurant address cannot be greater than 50 characters" });
       this.setState({ redirect: false });
       this.setState({ show: true });
       return;
     }
-  
+
     //verify email formatting
     if (!(/\S+@\S+\.\S+/.test(this.state.email))) {
       this.setState({ alertVariant: 'danger' });
@@ -137,13 +141,35 @@ class SignUp extends React.Component {
       return;
     }
 
+   
+      /*Validation for opening field.*/
+     else if (Number(this.state.opening) < 0 || Number(this.state.opening) > 2400) {
+      this.handleValidation("Valid opening time not entered.  Please enter a time between 0 and 2400.");
+    } else if (isNaN((this.state.opening))) {
+      this.handleValidation("No number entered for opening time.  Please enter a time between 0 and 2400.");
+    } else if (!Number.isInteger(parseFloat(this.state.opening))) {
+      this.handleValidation("No integer entered for opening time.  Please enter a time between 0 and 2400.");
+
+      /*Validation for closing field.*/
+    } else if (Number(this.state.closing) < 0 || Number(this.state.closing) > 2400) {
+      this.handleValidation("Valid closing time not entered.  Please enter a time between 0 and 2400.");
+    } else if (isNaN((this.state.closing))) {
+      this.handleValidation("No number entered for closing time.  Please enter a time between 0 and 2400.");
+    } else if (!Number.isInteger(parseFloat(this.state.closing))) {
+      this.handleValidation("No integer entered for closing time.  Please enter a time between 0 and 2400.");
+    }
+
     axios({
       method: 'put',
-      url: process.env.REACT_APP_DB + '/staff/register',
-      data: 'staff_id=' + this.state.staff_id + '&restaurant_id=' + this.state.restaurant_id
-        + '&first_name=' + this.state.first_name + '&last_name=' + this.state.last_name
-        + '&contact_num=' + this.state.contact_num + '&email=' + this.state.email
-        + '&position=' + this.state.position + '&password=' + this.state.password,
+      url: process.env.REACT_APP_DB + '/restaurant/new',
+      data: 
+        '&restaurant_name=' + this.state.restaurant_name +
+        '&restaurant_addr=' + this.state.restaurant_address + 
+        '&phone_number=' + this.state.contact_num +
+        '&email=' + this.state.email +
+        '&opening_time=' + this.state.opening + 
+        '&closing_time=' + this.state.closing + 
+        '&cuisine=' + this.state.cuisine,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
@@ -175,7 +201,7 @@ class SignUp extends React.Component {
   /* Used to show the correct alert after hitting save item */
   handleShow(success, message) {
     if (success) {
-      this.setState({ response: "Successfully created staff member: " + this.state.first_name });
+      this.setState({ response: "Successfully created restaurant: " + this.state.restaurant_name });
       this.setState({ alertVariant: 'success' });
     }
     else {
@@ -193,7 +219,7 @@ class SignUp extends React.Component {
     if (this.state.redirect === true) {
       return (
         <div>
-          
+
           <Alert show={this.state.show} variant={this.state.alertVariant}>
             {this.state.response}
           </Alert>
@@ -202,116 +228,146 @@ class SignUp extends React.Component {
         </div>
       );
     }
-      return (
-        <Container component="main" maxWidth="xs" className="p-3">
-          {/*alert if successful or unsuccessful*/}
+    return (
+      <Container component="main" maxWidth="xs" className="p-3">
+        {/*alert if successful or unsuccessful*/}
 
-          <Alert show={this.state.show} variant={this.state.alertVariant}>
-            {this.state.response}
-          </Alert>
-          <CssBaseline />
-          <div className={useStyles.paper}>
-            <div style={{ 'textAlign': 'center' }}>
-              {/* Lock icon on top */}
-              <div style={{ 'display': 'inline-block' }}>
-                <Avatar className={useStyles.avatar}>
-                  <LockOutlinedIcon />
-                </Avatar>
-              </div>
-              <Typography component="h1" variant="h5">
-                Resstaurant Sign Up
+        <Alert show={this.state.show} variant={this.state.alertVariant}>
+          {this.state.response}
+        </Alert>
+        <CssBaseline />
+        <div className={useStyles.paper}>
+          <div style={{ 'textAlign': 'center' }}>
+            {/* Lock icon on top */}
+            <div style={{ 'display': 'inline-block' }}>
+              <Avatar className={useStyles.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+            </div>
+            <Typography component="h1" variant="h5">
+              Restaurant Sign Up
 
               </Typography>
-              <br />
-            </div>
-            <form className={useStyles.form} onSubmit={this.handleSubmit}>
-              <Grid container spacing={2}>
-
-                 <Grid item xs={12}>
-                  <TextField onChange={this.onChange}
-                    variant="outlined"
-                    fullWidth
-                    id="restaurant_name"
-                    label="Restaurant Name"
-                    name="restaurant_name"
-                    value={this.state.restaurant_name}
-                    onChange={this.handleChange}
-                  />
-                  <div style={{ fontSize: 12, color: "red" }}>
-                    {this.state.staffIDError}
-                  </div>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField onChange={this.onChange}
-                    variant="outlined"
-                    fullWidth
-                    id="restaurant_address"
-                    label="Restaurant Address"
-                    name="restaurant_address"
-                    value={this.state.restaurant_address}
-                    onChange={this.handleChange}
-                  />
-                  <div style={{ fontSize: 12, color: "red" }}>
-                    {this.state.staffIDError}
-                  </div>
-                </Grid>
-
-                
-                
-                <Grid item xs={12}>
-                  <TextField onChange={this.onChange}
-                    variant="outlined"
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                  />
-                  <div style={{ fontSize: 12, color: "red" }}>
-                    {this.state.emailError}
-                  </div>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField onChange={this.onChange}
-                    variant="outlined"
-                    fullWidth
-                    id="phone"
-                    label="Contact Number"
-                    name="contact_num"
-                    value={this.state.contact_numb}
-                    onChange={this.handleChange}
-                  />
-                  <div style={{ fontSize: 12, color: "red" }}>
-                    {this.state.contactError}
-                  </div>
-                </Grid>
-              </Grid>
-              <br />
-
-              <Button onClick={this.handleSubmit}
-                type="submit"
-                fullWidth
-                variant="contained"
-                style={{ backgroundColor: '#0B658A', color: "#FFFFFF" }}
-                className={useStyles.submit}
-              >
-                Sign Up
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  {/* Create an account link */}
-                  <Link href="/" variant="body2" style={{ color: '#0B658A' }}>
-                    {"Already have an account? Sign In"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </form>
-
+            <br />
           </div>
-        </Container>
-      );
-    
+          <form className={useStyles.form} onSubmit={this.handleSubmit}>
+            <Grid container spacing={2}>
+
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="restaurant_name"
+                  label="Restaurant Name"
+                  name="restaurant_name"
+                  value={this.state.restaurant_name}
+                  onChange={this.handleChange}
+                />
+                <div style={{ fontSize: 12, color: "red" }}>
+                  {this.state.staffIDError}
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="restaurant_address"
+                  label="Restaurant Address"
+                  name="restaurant_address"
+                  value={this.state.restaurant_address}
+                  onChange={this.handleChange}
+                />
+
+              </Grid>
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+
+              </Grid>
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="phone"
+                  label="Contact Number"
+                  name="contact_num"
+                  value={this.state.contact_numb}
+                  onChange={this.handleChange}
+                />
+
+              </Grid>
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="cuisine"
+                  label="Cuisine"
+                  name="cuisine"
+                  value={this.state.cuisine}
+                  onChange={this.handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="opening"
+                  label="Opening Time"
+                  name="opening"
+                  value={this.state.opening}
+                  onChange={this.handleChange}
+                />
+                <i>*Please enter in military time</i>
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  id="closing"
+                  label="Closing Time"
+                  name="closing"
+                  value={this.state.closing}
+                  onChange={this.handleChange}
+                />
+                <i>*Please enter in military time</i>
+
+              </Grid>
+
+            </Grid>
+            <br />
+
+            <Button onClick={this.handleSubmit}
+              type="submit"
+              fullWidth
+              variant="contained"
+              style={{ backgroundColor: '#0B658A', color: "#FFFFFF" }}
+              className={useStyles.submit}
+            >
+              Sign Up
+              </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* Create an account link */}
+                <Link href="/" variant="body2" style={{ color: '#0B658A' }}>
+                  {"Already have an account? Sign In"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+
+        </div>
+      </Container>
+    );
+
   }
 }
 export default SignUp;
