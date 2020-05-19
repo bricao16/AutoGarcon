c8a919788bad3de621022b7fb487cd43f6f31383
HeesukJin <koch8697@stthomas.edu>
2020-05-19 02:49:05 -0500

    fixed open/close thing

--- app/src/main/java/auto_garcon/cartorderhistory/ShoppingCart.java
+++ app/src/main/java/auto_garcon/cartorderhistory/ShoppingCart.java
@@ -140,11 +140,7 @@ public class ShoppingCart extends AppCompatActivity implements NavigationView.On
             PlaceOrder.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int time = Calendar.getInstance(TimeZone.getTimeZone("America/Chicago")).get(Calendar.HOUR);
-
-                    if (time < 12) {
-                        time = time + 12;
-                    }
+                    int time = Calendar.getInstance(TimeZone.getTimeZone("America/Chicago")).get(Calendar.HOUR_OF_DAY);
 
                     String minute = Integer.toString(Calendar.getInstance(TimeZone.getTimeZone("America/Chicago")).get(Calendar.MINUTE));
 
@@ -153,7 +149,6 @@ public class ShoppingCart extends AppCompatActivity implements NavigationView.On
                     } else {
                         time = Integer.parseInt(Integer.toString(time) + Calendar.getInstance(TimeZone.getTimeZone("America/Chicago")).get(Calendar.MINUTE));
                     }
-
                     if (pref.getShoppingCart().getStartingHour() > time || pref.getShoppingCart().getEndingHour() < time) {
                         Toast.makeText(ShoppingCart.this, "The restaurant is currently closed.", Toast.LENGTH_LONG).show();
                     }
